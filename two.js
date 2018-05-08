2996ff779eb8fad422adf1ddd6f1495c1602df4e
fuchen5 <307553811@qq.com>
2018-05-08 11:21:01 +0800

    第一次提交

---
+++ two.js
@@ -0,0 +1,11 @@
+$(function() {
+    //点击注册局部切换
+    $(".weui-flex >div:eq(1)").on("click", function() {
+        console.log("222");
+        $(".main").toggle();
+        $(".login").toggle();
+    })
+
+})
+
+
